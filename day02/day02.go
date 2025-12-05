47c32cc98c5d895afce9e21056c57fce058196e7
seomac <icyveins@hotmail.com>
2025-12-06 07:41:16 +0800

    complete day02 part2
    ez

--- day02/day02.go
+++ day02/day02.go
@@ -7,17 +7,57 @@ import (
   "strconv"
 )
 
-func processRange(start, end int) int {
+func processRange(start, end int) (int, int) {
   sum := 0
+  sumany := 0
   for i := start; i <= end; i++ {
     isInvalid := checkNumber(i)
     // add
     if isInvalid {
       sum += i
     }
+
+    isInvalidAny := checkNumberAnyRepeat(i)
+    // add
+    if isInvalidAny {
+      sumany += i
+    }
   }
 
-  return sum
+  return sum, sumany
+}
+
+func checkNumberAnyRepeat(number int) bool {
+  // convert to string
+  s := strconv.Itoa(number)
+
+  // maximum length is half, +1 for safety
+  maxRepeatLen := len(s) / 2 + 1
+
+  for i := 1; i < maxRepeatLen; i++ {
+    // get the string
+    repeatString := s[:i]
+
+
+    // no point if not a multiple
+    if len(s) % i != 0 {
+      continue
+    }
+
+    // check if every string after is the same
+    found := true
+    for j := i; j < len(s); j += i {
+      if s[j : j+i] != repeatString {
+        found = false
+        break
+      }
+    }
+    if found {
+      return true
+    }
+  }
+
+  return false
 }
 
 func checkNumber(number int) bool {
@@ -57,6 +97,7 @@ func main() {
 
   // loop over ranges
   totalsum := 0
+  totalsumany := 0
   for _, s := range ranges {
     // split the string
     splitstrings := strings.Split(s, "-")
@@ -74,12 +115,14 @@ func main() {
 
     fmt.Printf("processing %d-%d\n", start, end)
 
-    rangesum := processRange(start, end)
+    rangesum, rangesumany := processRange(start, end)
     fmt.Printf("sum %d\n", rangesum)
 
     totalsum += rangesum
+    totalsumany += rangesumany
   }
 
   fmt.Printf("=== totalsum %d \n", totalsum)
+  fmt.Printf("=== totalsumany %d \n", totalsumany)
 
 }
